a67528c001aaeb4607ce823e8828bf14e42078b8
Jared Norman <hi@jarednorman.ca>
2012-10-03 19:40:46 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SquashgamEs::Application.config.secret_token = '009c32f5292b55a4b585962b14e10aa180166cce42be256e0caae11666b5b31d134c6d477f4cd1570ef9ac93b11a87cb600c8991363230ed2c8d144fd4a49c81'
